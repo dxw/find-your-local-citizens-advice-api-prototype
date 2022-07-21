5d6c6d66565f1279c92a055214c062f79c5b657e
Tom Hipkin <tomhipkin@gmail.com>
2022-07-21 18:24:25 +0100

    Create a basic office search endpoint
    It receives a postcode and goes the long way through each database to find the answer.
    
    Of course this should be optimised but this is just a prototype and there's talk of the data being joined at source.

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,16 @@
+class SearchController < ApplicationController
+  def show
+    postcode_query = "#{search_query.slice(0..2)} #{search_query.slice(3..6)}".upcase
+
+    # TODO: figure out how to protect users from writing back to this database
+    geolocation = Geolocation.find_by(postcode__c: postcode_query)
+    local_authority = LocalAuthority.find_by(id: geolocation.local_authority__c)
+    # TODO: Filter out outreaches once we understand how to interpret the `recordtypeid` column.
+    @offices = Office.where(local_authority__c: local_authority.id)
+    render json: @offices.to_json
+  end
+
+  def search_query
+    params.require(:id)
+  end
+end

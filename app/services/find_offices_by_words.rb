cab1d5e6c6c14a627d0b38f3ed17b97c98ab8909
Tom Hipkin <tomhipkin@gmail.com>
2022-08-04 17:22:45 +0100

    Convert raw sql to activerecord backed queries

--- app/services/find_offices_by_words.rb
+++ app/services/find_offices_by_words.rb
@@ -1,20 +1,17 @@
 class FindOfficesByWords
   def call(words_query:, limit: 10)
-    # Get all the local authorities with that word/s in the name
     local_authorities = InternalLocalAuthority.where('name ILIKE :query', query: "%#{words_query}%")
-    # Find all related offices
-    # offices = InternalOffice.where(local_authority__c: [local_authorities.pluck(:local_authority_foreign_key)])
 
-    sql = "
-      SELECT
-        offices.*,
-        null as eligible
-      FROM internal_offices AS offices
-      WHERE recordtypeid = '#{InternalOffice::OFFICE_RECORD_ID}'
-      AND name ILIKE '%#{words_query}%'
-      OR local_authority__c = '#{local_authorities.pluck(:local_authority_foreign_key)}'
-      LIMIT 10
-    "
+    sql = InternalOffice
+        .select("
+          *,
+          null as eligible
+        ")
+        .where(recordtypeid: InternalOffice::OFFICE_RECORD_ID)
+        .where('name ILIKE :query', query: '%#{words_query}%')
+        .or(InternalOffice.where(local_authority__c: local_authorities.pluck(:local_authority_foreign_key)))
+        .limit(10)
+        .to_sql
 
     results = ActiveRecord::Base.connection.execute(sql)
 

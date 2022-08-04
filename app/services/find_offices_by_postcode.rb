fd4672ac05b3ff3ebe156c0c3b2e3058856e2cf9
Tom Hipkin <tomhipkin@gmail.com>
2022-08-04 15:15:27 +0100

    Return office eligiblity if one can be determined

--- app/services/find_offices_by_postcode.rb
+++ app/services/find_offices_by_postcode.rb
@@ -14,7 +14,10 @@ class FindOfficesByPostcode
     limit = limit.present? ? "LIMIT #{limit}" : "LIMIT 10"
 
     sql = "
-      SELECT offices.*, ST_Distance(offices.lonlat,ST_GeogFromText('#{geolocation.lonlat}')) as distance_in_meters
+      SELECT
+        offices.*,
+        ST_Distance(offices.lonlat,ST_GeogFromText('#{geolocation.lonlat}')) as distance_in_meters,
+        local_authority__c = '#{geolocation.local_authority__c}' as eligible
       FROM internal_offices AS offices
       #{filters}
       ORDER BY lonlat <-> ST_GeogFromText('#{geolocation.lonlat}')

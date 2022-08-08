afaddaafea56cf5480155561281006c9bf9a9e99
Tom Hipkin <tomhipkin@gmail.com>
2022-08-08 11:34:38 +0100

    Update and tidy up AWS Glue scripts
    Keep the latest ones that only create CSV

--- aws-glue-geolocation-job.py
+++ aws-glue-geolocation-job.py
@@ -24,7 +24,7 @@ S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
     format="csv",
     connection_options={
-        "paths": ["s3://caew-find-lca-test/local-authorities/raw/"],
+        "paths": ["s3://caew-find-lca-test/geolocations/raw/"],
         "recurse": True,
     },
     transformation_ctx="S3bucket_node1",
@@ -34,12 +34,12 @@ S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
 ApplyMapping_node2 = ApplyMapping.apply(
     frame=S3bucket_node1,
     mappings=[
-        ("Id", "string", "local_authority_foreign_key", "string"),
+        ("Id", "string", "geolocation_foreign_key", "string"),
         ("Name", "string", "name", "string"),
-        ("BillingPostalCode", "string", "billingpostalcode", "string"),
-        ("BillingLatitude", "string", "billinglatitude", "string"),
-        ("BillingLongitude", "string", "billinglongitude", "string"),
-        ("RecordTypeId", "string", "recordtypeid", "string"),
+        ("Postcode__c", "string", "postcode__c", "string"),
+        ("Geolocation__Latitude__s", "string", "geolocation__latitude__s", "string"),
+        ("Geolocation__Longitude__s", "string", "geolocation__longitude__s", "string"),
+        ("Local_Authority__c", "string", "local_authority__c", "string"),
     ],
     transformation_ctx="ApplyMapping_node2",
 )
@@ -51,7 +51,7 @@ ApplyMapping_node2 = ApplyMapping_node2.coalesce(1)
 ## Insert into the Data catalog. DynamicFrame doesn't support overwrites but this action
 ## should ensure it matches the schema.
 S3bucket_node3 = glueContext.getSink(
-    path="s3://caew-find-lca-test/local-authorities/csv/versions/",
+    path="s3://caew-find-lca-test/geolocations/csv/versions/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
@@ -59,22 +59,22 @@ S3bucket_node3 = glueContext.getSink(
     transformation_ctx="S3bucket_node3",
 )
 S3bucket_node3.setCatalogInfo(
-    catalogDatabase="salesforce_data", catalogTableName="local_authorities"
+    catalogDatabase="salesforce_data", catalogTableName="geolocations"
 )
 S3bucket_node3.setFormat("csv")
 S3bucket_node3.writeFrame(ApplyMapping_node2)
 
 ## Write the final file with overwrite to ensure one file remains for the API
 df = ApplyMapping_node2.toDF()
-df.write.mode('overwrite').csv('s3://caew-find-lca-test/local-authorities/csv/', header=True, quoteAll=True, escape="\\")
+df.write.mode('overwrite').csv('s3://caew-find-lca-test/geolocations/csv/', header=True, quoteAll=True, escape="\\")
 
 # Copy the file across to a stable filename
 import boto3
 bucketname = "caew-find-lca-test"
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket(bucketname)
-source = "local-authorities/csv/"
-target = "local-authorities/csv/renamed"
+source = "geolocations/csv/"
+target = "geolocations/csv/renamed"
 for obj in my_bucket.objects.filter(Prefix=source):
     source_filename = (obj.key).split('/')[-1]
     copy_source = {
@@ -83,6 +83,5 @@ for obj in my_bucket.objects.filter(Prefix=source):
     }
     target_filename = "{}/{}".format(target, "data.csv")
     s3.meta.client.copy(copy_source, bucketname, target_filename)
-
 
 job.commit()
